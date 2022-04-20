5a6f4f78ceb886593ea6ae0176bcba64d0cc6de1
Abdul535 <syedabdullah535@gmail.com>
2022-04-20 23:45:35 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Jumbotron from './Jumbotron';
+import Feature from './Feature';
+import Cards from './Cards';
+import Footer from './Footer';
+
+const App = () => (
+    <>
+       <Navbar />
+        <Jumbotron />
+        <Feature />
+        <Cards />
+        <Footer />
+    </>
+  )
+
+
+export default App;
+
